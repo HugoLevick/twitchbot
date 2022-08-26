d14989762fc6e8dd8ab1608afccf2f35cf72ca5a
HugoLevick <hlals12346@gmail.com>
2022-08-26 16:35:45 -0500

    Update mySQLCredentials.js

--- mySQLCredentials.js
+++ mySQLCredentials.js
@@ -1,6 +1,8 @@
 const mysqlCredentials = {
-  host: "dpg-cc4jep1gp3jiap6hdotg-a:5432",
+  host: "dpg-cc4jep1gp3jiap6hdotg-a",
+  port: "5432",
   user: "root",
   password: "TwuK2hKKdw3YOFE7IN8kwm8jVMyVEt6m",
+  connectTimeout: 10000,
 };
 export default mysqlCredentials;
